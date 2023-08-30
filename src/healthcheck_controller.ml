473dae855b2d24802deb80951cdb656f584f60ba
Jake Helfrich <jakenichelfrich@gmail.com>
2023-08-30 18:18:18 +0800

    Added System Time to Healthcheck response

--- src/healthcheck_controller.ml
+++ src/healthcheck_controller.ml
@@ -2,12 +2,16 @@ open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
 type healthcheck_response = {
   status: string;
+  system_time: float
 } [@@deriving yojson];;
 
 
 let healthcheck_controller = [
   Dream.get "" (fun _ -> 
-    let resp = { status = "pass" } in
+    let resp = { 
+      status = "pass";
+      system_time = Unix.time () 
+    } in
     resp
     |> yojson_of_healthcheck_response
     |> Yojson.Safe.to_string
