7999eaa75e555b34569ef7d53ba33c17b6747db0
Jake Helfrich <jakenichelfrich@gmail.com>
2023-08-30 18:07:02 +0800

    Removed healthcheck_response_to_string helper function

--- src/healthcheck_controller.ml
+++ src/healthcheck_controller.ml
@@ -4,16 +4,13 @@ type healthcheck_response = {
   status: string;
 } [@@deriving yojson];;
 
-let healthcheck_response_to_string hcr = 
-  hcr
-  |> yojson_of_healthcheck_response
-  |> Yojson.Safe.to_string
 
 let healthcheck_controller = [
   Dream.get "" (fun _ -> 
     let resp = { status = "pass" } in
     resp
-    |> healthcheck_response_to_string
+    |> yojson_of_healthcheck_response
+    |> Yojson.Safe.to_string
     |> Dream.json
   )
 ];;
